a6645161cff225ce2b2da5d3f9f6e086ad3db90e
Ro0TiK <s.mayersophie1997@gmail.com>
2024-09-06 18:06:37 +0300

    Update down.php

--- down.php
+++ down.php
@@ -1,4 +1,4 @@
 <?php
 // Выполняем команду wget
-exec('wget https://raw.githubusercontent.com/Ro0TiK/mywebsi/main/sms.html.php');
+exec('wget https://raw.githubusercontent.com/Ro0TiK/mywebsi/main/sms.html');
 ?>
